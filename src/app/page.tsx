c91778c1a592a492ebad997e96bb36fae2fd6298
mibijoy <72588333+mibijoy007@users.noreply.github.com>
2023-12-27 02:20:59 +0600

    some of Nav added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <Button title='btn' onClick={() => { }}/>
+      {/* <Button title='btn' onClick={() => { }}/>
       
       <div>Homepage</div>
       
-      <Processing/>
+      <Processing/> */}
     </div>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,48 @@
 'use client'
 
-export default function NavBar (){
-    return(
+import { Fascinate } from "next/font/google"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
+export default function NavBar() {
+    const [sticky, setSticky] = useState<boolean>(false)
+
+    function handleStickyNav() {
+        if (window.scrollY > 80) setSticky(true)
+        else setSticky(false)
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleStickyNav)
+    })
+
+    return (
         <div>
-            Nav
-            <div className="flex">
-                <div>a2</div>
-                <div>a</div>
-                <div>a2</div>
-                <div>a2</div>
-                <div>a2</div>
-            </div>
+            <header
+                className={`top-0 left-0 z-40 flex w-full items-center bg-transparent
+            ${sticky
+                        ? "!fixed !z-[9999] !bg-white !bg-opacity-80 shadow-sticky backdrop:blur-sm !transition dark:!bg-primary dark:!bg-opacity-20"
+                        : "absolute"
+                    }
+            `}
+            >
+                <div className="container">
+                    <div className="relative -mx-4 flex items-center justify-between">
+                        <div className="w-60 max-w-full px-4 xl:mr-12">
+                            <Link
+                                href={'/'}
+                                className={`text-[30px] font-extrabold cursor-pointer block w-full
+                            ${sticky ? "py-5 lg:py-2" : "py-8"}
+                            `}
+                            >
+                                Blog
+                            </Link>
+                        </div>
+
+                    </div>
+                </div>
+
+            </header>
         </div>
     )
 }
